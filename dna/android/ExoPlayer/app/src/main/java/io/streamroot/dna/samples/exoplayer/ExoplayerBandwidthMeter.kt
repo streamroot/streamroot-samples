05e3ed175d5e78364d5281ed47382400a7c35c9e
Boris Borgobello <boris.borgobello@centurylink.com>
2021-06-21 18:11:34 +0200

    Added new initial value for BW

--- dna/android/ExoPlayer/app/src/main/java/io/streamroot/dna/samples/exoplayer/ExoplayerBandwidthMeter.kt
+++ dna/android/ExoPlayer/app/src/main/java/io/streamroot/dna/samples/exoplayer/ExoplayerBandwidthMeter.kt
@@ -1,18 +1,21 @@
 package io.streamroot.dna.samples.exoplayer
 
+import android.content.Context
 import android.os.Handler
 import com.google.android.exoplayer2.upstream.BandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.TransferListener
 import io.streamroot.dna.core.BandwidthListener
 import java.util.concurrent.atomic.AtomicLong
 
-class ExoPlayerBandwidthMeter : BandwidthMeter, BandwidthListener {
+class ExoPlayerBandwidthMeter(context: Context) : BandwidthMeter, BandwidthListener {
 
     override fun getTransferListener(): TransferListener? = null
     override fun addEventListener(eventHandler: Handler, eventListener: BandwidthMeter.EventListener) { }
     override fun removeEventListener(eventListener: BandwidthMeter.EventListener) { }
 
-    private val estimatedBandwidth = AtomicLong(0L)
+    private val estimatedBandwidth
+        = AtomicLong(DefaultBandwidthMeter.Builder(context).build().bitrateEstimate)
 
     override fun getBitrateEstimate(): Long {
         return estimatedBandwidth.get()
